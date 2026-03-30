6cabaa9b8d2118032cd575b98b0289c6b73c3530
pateldha <pateldha@us.ibm.com>
2026-03-30 13:34:14 -0400

    revising the scenarios and data unification

--- experimental_scenarios/metrotrain_compressor/covert_to_jsonl.py
+++
@@ -1,9 +0,0 @@
-import json
-
-# Replace 'input.json' and 'output.jsonl' with your filenames
-with open('scenario_revised.json', 'r') as f:
-    data = json.load(f)
-
-with open('compressor_utterance.jsonl', 'w') as f:
-    for entry in data:
-        f.write(json.dumps(entry) + '\n')
